66fcac0b2c78b9f6e0616ee15aed4af59af7458f
ZacRobinson84 <zacrobinson84@gmail.com>
2021-12-30 15:15:53 -0500

    add random thing to cross generator

--- messageGenerator.js
+++ messageGenerator.js
@@ -16,3 +16,21 @@ const randomAnimal = () => {
   ];
   return animals[Math.floor(Math.random() * animals.length)];
 };
+
+// Generate a random thing to be crossed
+const randCrossObject = () => {
+  const crossObj = [
+    "road",
+    "lake",
+    "bridge",
+    "ocean",
+    "dessert",
+    "plains",
+    "forest",
+    "bike lane",
+    "highway",
+    "train tracks",
+    "galaxy",
+  ];
+  return crossObj[Math.floor(Math.random() * crossObj.length)];
+};
