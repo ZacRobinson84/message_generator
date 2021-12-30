6cc174f7c1077b2b967f4b2261ffff1efba4921e
ZacRobinson84 <zacrobinson84@gmail.com>
2021-12-30 15:36:21 -0500

    add random answer generator

--- messageGenerator.js
+++ messageGenerator.js
@@ -13,6 +13,7 @@ const randomAnimal = () => {
     "rhinoceros",
     "bumble bee",
     "spider",
+    "lion",
   ];
   return animals[Math.floor(Math.random() * animals.length)];
 };
@@ -33,4 +34,21 @@ const randCrossObject = () => {
     "galaxy",
   ];
   return crossObj[Math.floor(Math.random() * crossObj.length)];
+};
+
+// Generate a random answer
+const randAnswer = () => {
+  const answer = [
+    "to get to the other side!",
+    "to go to the library and do some research for their history project!",
+    "they thought they saw a very large burrito on the other side, but were disapointed to find it was only a small child.",
+    "that's where there work was. And although they weren't working that day, they had forgotten there umbrella there and it was starting to rain.",
+    "to get to the other PRIDE!",
+    "it was their TOP SECRET MISSION. Now I'm sorry, but we will have to have you killed.",
+    "just to eat some bananas.",
+    "maybe they wanted to... eat some bananas.",
+    `because the ${randomAnimal()} was chasing them!`,
+    `because they thought they saw a delicioius ${randomAnimal()}!`,
+  ];
+  return answer[Math.floor(Math.random() * answer.length)];
 };
